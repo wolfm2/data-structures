d849a2fe73ea7f8562f3d6946b63a38b64b7cad8
wolfm2 <wolfm2@newschool.edu>
2018-10-17 20:32:44 -0400

    data dir

--- week_6/main.js
+++ week_6/main.js
@@ -3,11 +3,7 @@
 var fs = require('fs');
 var cheerio = require('cheerio');
 
-var content = fs.readFileSync('aaInfo/m04.html');
-// load `content` into a cheerio object
-var $ = cheerio.load(content);
-
-data = [];  // structure I will fill
+var data;
 
 // days 
 function getDays (elem) {
@@ -92,37 +88,39 @@ function getAddress (elem) {
 // special interest
 // go through all rows
 
-$('tr').each(function(i, elem) {
-	d = {};
-	var adr = getAddress(elem);
-	console.log(adr);
-	var ttl = getTitle(elem);
-	var wch = getWchair(elem);
-	var day = getDays(elem);
-	//console.log(day)
-	var tim = getTimes(elem);
-	var det = getDetails(elem);
-	var mty = getMType(elem);
-	//console.log(mty);
-	// mty = [1,2,3,4,5,6,7,7,8,8,7,6,6];
-	
-	if (adr != null) {
-		d['address'] = adr[0];
-		if (adr.length > 1)
-		  d['adrMeta'] = adr.slice(1);
-		meetInfo = [];  
-		day.forEach((d,i) => {
-			meetInfo.push([d, tim[i*2], tim[i*2+1], mty[i]]);
-			});
-		d['title'] = ttl;	
-	  d['wheelc'] = wch;
-	  d['meetings'] = meetInfo;
-	  d['details'] = det;
-		data.push(d);
-	}
-});
+function parseData () {
+	$('tr').each(function(i, elem) {
+		d = {};
+		var adr = getAddress(elem);
+		// console.log(adr);
+		var ttl = getTitle(elem);
+		var wch = getWchair(elem);
+		var day = getDays(elem);
+		//console.log(day)
+		var tim = getTimes(elem);
+		var det = getDetails(elem);
+		var mty = getMType(elem);
+		//console.log(mty);
+		// mty = [1,2,3,4,5,6,7,7,8,8,7,6,6];
+		
+		if (adr != null) {
+			d['address'] = adr[0];
+			if (adr.length > 1)
+				d['adrMeta'] = adr.slice(1);
+			meetInfo = [];  
+			day.forEach((d,i) => {
+				meetInfo.push([d, tim[i*2], tim[i*2+1], mty[i]]);
+				});
+			d['title'] = ttl;	
+			d['wheelc'] = wch;
+			d['meetings'] = meetInfo;
+			d['details'] = det;
+			data.push(d);
+		}
+	});
+}
 
-console.log(JSON.stringify(data));
+// console.log(JSON.stringify(data));
 
 // look for td element with unique style attribute
 //~ $('td[style="border-bottom:1px solid #e3e3e3; width:260px"]').each(function(i, elem) {
@@ -141,4 +139,18 @@ console.log(JSON.stringify(data));
     //~ // console.log("'" + text + "'");
 //~ });
 
-// fs.writeFileSync('data.json', JSON.stringify(data));
+for (var i = 1; i <= 10; i++) {
+	var content;
+	if (i < 10)
+		content = fs.readFileSync(`aaInfo/m0${i}.html`);
+	else
+		content = fs.readFileSync(`aaInfo/m${i}.html`);
+	  
+	var $ = cheerio.load(content);
+	
+	data = [];  // structure I will fill
+	
+	parseData();
+	
+	fs.writeFileSync(`data/d${i}.json`, JSON.stringify(data));
+}
