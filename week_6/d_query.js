1a47e5b1485a9edb2a0ff67683d873ddbc4cbb91
wolfm2 <wolfm2@newschool.edu>
2018-10-20 17:58:52 -0400

    init files

---
+++ week_6/d_query.js
@@ -0,0 +1,32 @@
+// npm install aws-sdk
+var AWS = require('aws-sdk');
+AWS.config = new AWS.Config();
+AWS.config.accessKeyId = process.env.AWS_ID;
+AWS.config.secretAccessKey = process.env.AWS_KEY;
+AWS.config.region = "us-east-1";
+
+var dynamodb = new AWS.DynamoDB();
+
+var params = {
+    TableName : "aarondiary",
+    KeyConditionExpression: "#tp = :topicName and dt between :minDate and :maxDate", // the query expression
+    ExpressionAttributeNames: { // name substitution, used for reserved words in DynamoDB
+        "#tp" : "topic"
+    },
+    ExpressionAttributeValues: { // the query values
+        ":topicName": {S: "work"},
+        ":minDate": {N: new Date("September 1, 2018").valueOf().toString()},
+        ":maxDate": {N: new Date("October 16, 2018").valueOf().toString()}
+    }
+};
+
+dynamodb.query(params, function(err, data) {
+    if (err) {
+        console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Query succeeded.");
+        data.Items.forEach(function(item) {
+            console.log("***** ***** ***** ***** ***** \n", item);
+        });
+    }
+});
